b2ee4242fadcb6887d836a904797dca03166e0e9
Daniel Garofalo <danielgarofalo@Daniels-MacBook-Pro.local>
2018-08-31 18:08:21 -0500

    initial xcode project

---
+++ DiggingDeeperAutoLayout/DiggingDeeperAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiggingDeeperAutoLayout
+//
+//  Created by Daniel Garofalo on 8/31/18.
+//  Copyright © 2018 Daniel Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
